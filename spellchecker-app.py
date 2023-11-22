2d82787a69638e388b6093c673746b2af7630bfa
Abubaker911 <128919027+Abubaker911@users.noreply.github.com>
2023-11-22 08:55:44 +0500

    Update spellchecker-app.py

--- spellchecker-app.py
+++ spellchecker-app.py
@@ -1,26 +1,48 @@
+from gingerit.gingerit import GingerIt
 import streamlit as st
+import pandas as pd
+import time
 
 
-spell = SpellChecker()
+def process_text(text):
+    parser = GingerIt()
+    result_dict = parser.parse(text)
 
-from spellchecker import SpellChecker
-def spell_check(text):
-    words = text.split()
-    corrected_words = [spell.correction(word) for word in words]
-    corrected_text = ' '.join(corrected_words)
-    return corrected_text
-
-def main():
-    st.title("Spell Checker App")
-    st.write("Enter a sentence to check and correct spelling:")
-
-    user_input = st.text_area("Input your text:")
+    corrections = result_dict['corrections']
+    df_data = []
+    for correction in corrections:
+        df_data.append({
+            'index_num': correction['start'],
+            'incorrect_words': correction['text'],
+            'correct_words': correction['correct'],
+            'definition': correction['definition']
+    })
     
-    if st.button("Check and Correct"):
-        result = spell_check(user_input)
-        st.success("Corrected Text:")
-        st.write(result)
+    df = pd.DataFrame(df_data).sort_values('index_num')
+    return df, result_dict['result']
 
-if __name__ == "__main__":
-    main()
+def all_results():
+    st.title('Grammar & Spell Checker in Python')
+    text = st.text_area("Enter text:")
+    
+    if st.button('Correct Sentence'):
+        if not text:
+            st.write("Please enter text for checking spelling")
+        else:
+            df, corrected_text = process_text(text)
+            progress_text = "Operation in progress. Please wait."
+            my_bar = st.progress(0, text=progress_text)
 
+            for percent_complete in range(100):
+                time.sleep(0.1)
+                my_bar.progress(percent_complete + 1, text=progress_text)
+            st.write("Parsing completed successfully")
+            st.balloons()
+            st.markdown('Corrected Sentence - ' + corrected_text)
+            st.button("Corrections")
+            st.dataframe(df, use_container_width=True)
+
+
+if __name__ == '__main__':
+    all_results()
+    
